e79f99b53ae5a1048c511b096cbff39b9b8e3e65
Hayden <hayden.fuller@gmail.com>
2020-07-23 12:43:29 +1000

    removed console log

--- src/components/DownloadContactListButton.tsx
+++ src/components/DownloadContactListButton.tsx
@@ -37,7 +37,6 @@ export function DownloadContactListButton (props: DownloadButtonProps) {
     }).filter((row) => row).join('')
 
     const csvData = `${csvHeader}${csvRows}`
-    console.log(csvData.length)
     const resultsBlob = new Blob(
         [csvData],
         {
